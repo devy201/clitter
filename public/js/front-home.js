c048bd004d4ad794631d4ee605d478ca22104f8f
devy201 <devy201@gmail.com>
2013-03-11 18:33:12 +0200

    added delete task visualisation

--- public/js/front-home.js
+++ public/js/front-home.js
@@ -6,15 +6,100 @@
  * To change this template use File | Settings | File Templates.
  */
 
+function isVisible(){
+    $('.task').each(function(i){
+        if(!$(this).hasClass('visible')){
+            $(this).find('.task-text').hide();
+        }
+    });
+}
 
-$(function(){
+function showTaskNotes(task, button){
+    task.slideToggle('fast');
+    if(button.hasClass('open')){
+        button.removeClass('open');
+    }
+    else{
+        button.addClass('open');
+    }
+}
 
+function addTask(){
+    var taskTitle = $('.new-task-title').val();
+    var taskText = $('.new-task-area').val();
+
+    /* TEMPORARY BLOCK */
+    var taskBlock = $('.task-box #today');
+
+    taskBlock.append('<div class="task"><div class="task-title"><h4>'+taskTitle+'</h4><div class="btn-cont">' +
+        '<button class="ctrl-task"></button>' +
+        '<button class="edit-task"></button>' +
+        '<button class="end-task"></button></div></div>' +
+        '<div class="task-text">'+taskText+'</div></div>');
+
+    showButtons();
+    isVisible();
+    $('.ctrl-task').off('click').on('click', function(){
+        showTaskNotes($(this).parents().eq(2).find('.task-text'), $(this));
+    });
+    /*END TEMPORARY*/
+}
+
+function deleteTask(task){
+    task.find('.task-text').slideUp('fast');
+    task.addClass('deleted').removeClass('visible').delay(300).slideUp('fast', function(){
+        $('#ended').append(task);
+        task.slideDown()
+    });
+    task.find('.ctrl-task').removeClass('open');
+
+
+}
+
+function showButtons(){
     $('.task-title')
         .on('mouseenter', function(){
             $(this).children('.btn-cont').fadeIn('fast');
         })
         .on('mouseleave', function(){
             $(this).children('.btn-cont').fadeOut('fast');
+        });
+}
+
+function endTaskButtons(){
+    return '<button id="end-yes" class="btn" onclick="endTask($(this), true)" style="margin-right: 28px;">Yes</button>'+
+        '<button id="end-no" class="btn" onclick="endTask($(this))">No</button>';
+}
+
+function endTask($object, isEnded){
+    if(isEnded){
+        deleteTask($object.parents().eq(5));
+    }
+    $('.end-task').popover('hide');
+}
+
+
+$(function(){
+
+    isVisible();
+    showButtons();
+    $('.end-task').each(function(){
+        $(this).popover({
+            title: "End task?",
+            content: endTaskButtons(),
+            html: true,
+            trigger: 'click',
+            placement: 'top'
         })
+    });
+
+    $('.ctrl-task').on('click', function(){
+        showTaskNotes($(this).parents().eq(2).find('.task-text'), $(this));
+    });
+
+
+    $('#add-task').on('click', function(){
+        addTask();
+    })
 
 });
